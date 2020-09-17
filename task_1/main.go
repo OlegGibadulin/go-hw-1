757bd6b4b11421cf933cf90a95575f0ec8486ec8
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-09-17 14:32:41 +0300

    Add Uniq implementetion

--- task_1/main.go
+++ task_1/main.go
@@ -11,6 +11,35 @@ import (
 	"task_1/uniq"
 )
 
+func main() {
+	options := new(uniq.Options)
+	parseFlags(options)
+	if err := checkFlags(options); err != nil {
+		log.Println(err)
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+
+	inputFilename := flag.Arg(0)
+	input, err := readFromFile(inputFilename)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	uniqRes, err := uniq.Execute(input, options)
+	if err != nil {
+		log.Println(err)
+		os.Exit(3)
+	}
+
+	outputFilename := flag.Arg(1)
+	if err := writeToFile(outputFilename, uniqRes); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
 func trueCount(b ...bool) uint {
 	var count uint = 0
 	for _, v := range b {
@@ -29,15 +58,14 @@ func checkFlags(options *uniq.Options) error {
 	return nil
 }
 
-func parseFlags(options *uniq.Options) error {
+func parseFlags(options *uniq.Options) {
 	flag.BoolVar(&options.NeedCount, "c", false, "display count of repeated lines")
 	flag.BoolVar(&options.OnlyRepeated, "d", false, "display only repeated lines")
 	flag.BoolVar(&options.OnlyUnique, "u", false, "display only unique lines")
-	flag.UintVar(&options.SkipFieldsCount, "f", 0, "skip N fields (group of characters, delimited by whitespace)")
-	flag.UintVar(&options.SkipCharsCount, "s", 0, "skip N characters")
+	flag.IntVar(&options.SkipFieldsCount, "f", 0, "skip N fields (group of characters, delimited by whitespace)")
+	flag.IntVar(&options.SkipCharsCount, "s", 0, "skip N characters")
 	flag.BoolVar(&options.IgnoreCase, "i", false, "ignore case")
 	flag.Parse()
-	return checkFlags(options)
 }
 
 func readFromFile(filename string) ([]string, error) {
@@ -78,38 +106,10 @@ func writeToFile(filename string, src []string) error {
 	}
 
 	for _, s := range src {
-		_, err := io.WriteString(outputFile, s)
+		_, err := io.WriteString(outputFile, s+"\n")
 		if err != nil {
 			return err
 		}
 	}
 	return nil
-}
-
-func main() {
-	options := new(uniq.Options)
-	if err := parseFlags(options); err != nil {
-		log.Println(err)
-		flag.PrintDefaults()
-		os.Exit(2)
-	}
-
-	inputFilename := flag.Arg(0)
-	input, err := readFromFile(inputFilename)
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
-
-	uniqRes, err := uniq.Execute(input, options)
-	if err != nil {
-		log.Println(err)
-		os.Exit(3)
-	}
-
-	outputFilename := flag.Arg(1)
-	if err := writeToFile(outputFilename, uniqRes); err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
 }
--- task_1/uniq/uniq.go
+++ task_1/uniq/uniq.go
@@ -1,16 +1,117 @@
 package uniq
 
-import ()
+import (
+	"strconv"
+	"strings"
+)
 
 type Options struct {
 	NeedCount       bool
 	OnlyRepeated    bool
 	OnlyUnique      bool
-	SkipFieldsCount uint
-	SkipCharsCount  uint
+	SkipFieldsCount int
+	SkipCharsCount  int
 	IgnoreCase      bool
 }
 
 func Execute(src []string, options *Options) ([]string, error) {
-	return nil, nil
+	knownStrings := make(map[string]int)
+	var uniqStrings []string
+
+	for _, s := range src {
+		template := getStringTemplate(options, s)
+		if _, ok := knownStrings[template]; ok {
+			knownStrings[template]++
+		} else {
+			uniqStrings = append(uniqStrings, s)
+			knownStrings[template] = 1
+		}
+	}
+
+	uniqStrings = checkOptions(options, knownStrings, uniqStrings)
+
+	return uniqStrings, nil
+}
+
+func getStringTemplate(options *Options, s string) string {
+	template := s
+
+	if options.IgnoreCase {
+		template = strings.ToLower(template)
+	}
+
+	if options.SkipFieldsCount != 0 {
+		words := strings.Split(template, " ")
+		if len(words) <= options.SkipFieldsCount {
+			template = ""
+		} else {
+			template = strings.Join(words[options.SkipFieldsCount:], " ")
+		}
+	}
+
+	if options.SkipCharsCount != 0 {
+		if len(template) <= options.SkipCharsCount {
+			template = ""
+		} else {
+			template = template[options.SkipCharsCount:]
+		}
+	}
+	return template
+
+}
+
+func getStringCount(options *Options, knownStrings map[string]int, s string) int {
+	template := getStringTemplate(options, s)
+	if options.IgnoreCase {
+		template = strings.ToLower(s)
+	}
+	return knownStrings[template]
+}
+
+func checkOptions(options *Options, knownStrings map[string]int, uniqStrings []string) []string {
+	uniqStrings = checkNeedCount(options, knownStrings, uniqStrings)
+	uniqStrings = checkOnlyRepeated(options, knownStrings, uniqStrings)
+	uniqStrings = checkOnlyUnique(options, knownStrings, uniqStrings)
+	return uniqStrings
+}
+
+func checkNeedCount(options *Options, knownStrings map[string]int, uniqStrings []string) []string {
+	var res []string
+	if options.NeedCount {
+		for _, s := range uniqStrings {
+			count := getStringCount(options, knownStrings, s)
+			resString := strconv.Itoa(count) + " " + s
+			res = append(res, resString)
+		}
+		return res
+	}
+	return uniqStrings
+}
+
+func checkOnlyRepeated(options *Options, knownStrings map[string]int, uniqStrings []string) []string {
+	var res []string
+	if options.OnlyRepeated {
+		for _, s := range uniqStrings {
+			count := getStringCount(options, knownStrings, s)
+			if count > 1 {
+				res = append(res, s)
+			}
+		}
+		return res
+	}
+	return uniqStrings
+}
+
+func checkOnlyUnique(options *Options, knownStrings map[string]int, uniqStrings []string) []string {
+	var res []string
+	if options.OnlyUnique {
+		for _, s := range uniqStrings {
+			count := getStringCount(options, knownStrings, s)
+			if count == 1 {
+				res = append(res, s)
+			}
+		}
+		return res
+	}
+	return uniqStrings
 }
